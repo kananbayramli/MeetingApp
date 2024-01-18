e979574c23b7b7d282000e1f210aaac7b628189a
kananbayramli <bayramli-kenan@mail.ru>
2024-01-18 21:27:22 +0400

    test Model Created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,30 @@
+using MeetingApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(){
-            return View();
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+            // var salamlama = saat < 12 ? "Sabahiniz Xeyir" : "Her Vaxtiniz Xeyir";
+            // return View(model: salamlama);
+
+
+            ViewBag.Salamlama = saat < 12 ? "Sabahiniz Xeyir" : "Her Vaxtiniz Xeyir";
+            ViewBag.UserName = "Kenan";
+
+            var meetingInfo = new MeetingInfo()
+            {
+                Id = 1,
+                Location = "Baku, Flame Tower, 10a",
+                Date = new DateTime(2024, 01, 31, 21, 0, 0),
+                NumberOfPeople = 200
+
+            };
+
+            return View(meetingInfo);
         }
     }
 }
