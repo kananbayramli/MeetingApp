752f1cbbd30f08f910649c4e495396864e2aaecf
kananbayramli <bayramli-kenan@mail.ru>
2024-01-18 18:58:34 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+
+app.Run();
